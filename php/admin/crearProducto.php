7ebab165afb00e78becb9b30c6b907dbe449a4e2
Solaquin <145218692+Solaquin@users.noreply.github.com>
2025-05-29 00:24:11 -0500

    updated crearProducto y editarProducto

--- php/admin/crearProducto.php
+++ php/admin/crearProducto.php
@@ -1,4 +1,11 @@
 <?php
+
+    session_start();
+    if (!isset($_SESSION['email'])) {
+        header('Location: login.php');
+        exit();
+    }
+
     if($_SERVER['REQUEST_METHOD'] == 'POST') 
     {
         // Include the database connection
