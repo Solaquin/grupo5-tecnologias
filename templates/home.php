65dbb6e3329534d273451e955e4c2d51f9b91be2
Solaquin <145218692+Solaquin@users.noreply.github.com>
2025-05-28 21:09:47 -0500

    change html for php

---
+++ templates/home.php
@@ -0,0 +1,67 @@
+<?php include('php/includes/header.php'); ?>
+
+  <!-- Hero -->
+  <section class="hero">
+    <div class="hero-text">
+      <h1>BILLETERAS BICUR</h1>
+      <p>Encuentra tu estilo perfecto</p>
+    </div>
+  </section>
+
+  <!-- Intro -->
+  <section class="intro">
+    <h2>MARROQUINERÍA BICUR</h2>
+    <p>
+      Cada pieza que creamos es una fusión de tradición, calidad y diseño. En Bicur, el cuero toma forma a través del talento artesanal, dando vida a accesorios elegantes, funcionales y llenos de historia. Más que marroquinería, es pasión hecha a mano.
+    </p>
+  </section>
+
+  <section class="categorias">
+  <!-- Categoría Mujer -->
+  <div class="cat mujer">
+    <div class="overlay">
+      <img src="../sources/mujer.jpg" alt="Categoría Mujer">
+      <a href="frame4.html" class="btn btn-outline-burgundy btn-borde-blanco">Ver Mujer</a>
+    </div>
+  </div>
+
+  <!-- Categoría Hombre -->
+  <div class="cat hombre">
+    <div class="overlay">
+      <img src="../sources/tarjetero.jpg" alt="Categoría Hombre">
+      <a href="#" class="btn btn-outline-burgundy btn-borde-blanco">Ver Hombre</a>
+    </div>
+  </div>
+</section>
+
+
+
+  <!-- Productos -->
+  <section class="destacados">
+    <h2>PRODUCTOS DESTACADOS</h2>
+    <div class="productos">
+      <div class="producto">
+        <img src="../sources/tarjeterodoble.jpg" alt="Tarjetero Magno">
+        <p>Tarjetero de hombre cuero Magno</p>
+        <span>$40.000</span>
+      </div>
+      <div class="producto">
+        <img src="../sources/tarjeterosencillo.jpg" alt="Tarjetero Bravus">
+        <p>Tarjetero de hombre cuero Bravus</p>
+        <span>$25.000</span>
+      </div>
+      <div class="producto">
+        <img src="../sources/damagrande.jpg" alt="Billetera Zafiro">
+        <p>Billetera de mujer cuero Zafiro</p>
+        <span>$45.000</span>
+      </div>
+      <div class="producto">
+        <img src="../sources/damamariposas.jpg" alt="Billetera Aurora">
+        <p>Billetera de mujer cuero Aurora</p>
+        <span>$70.000</span>
+      </div>
+    </div>
+    <p class="slogan">Bicur: cuero artesanal con historia y esencia.</p>
+  </section>
+
+<?php include('php/includes/footer.php'); ?>
